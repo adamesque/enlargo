c17cb8bdc4ad64f03274348e52223aa71e40afdf
Adam Luikart <me@adamluikart.com>
2011-11-11 00:47:44 -0600

    Do some very rudimentary bounds checking to keep images from falling off the page.

--- js/enlargo.jquery.js
+++ js/enlargo.jquery.js
@@ -2,9 +2,9 @@
 ;(function ($, window, document, undefined) {
 	var pluginName = 'enlargo',
 		defaults = {
-			duration: 200
-		},
-		enlargoTimer;
+			duration: 200,
+			margin: 20
+		};
 
 	function Enlargo( element, options ) {
 		this.element = element;
@@ -38,9 +38,7 @@
 
 		this.fullContainer = $('<div class="enlargo-container"/>')
 			.css({
-				left: this.offset.left,
 				height: this.height,
-				top: this.offset.top,
 				width: this.width
 			})
 			.appendTo(document.body);
@@ -53,22 +51,47 @@
 		}).appendTo(this.fullContainer);
 
 		this.el.on("click mouseenter", function () {
-			window.clearTimeout(enlargoTimer);
-			enlargoTimer = window.setTimeout($.proxy($this.expand, $this), $this.options.duration);
+			window.clearTimeout($this.enlargoTimer);
+			$this.enlargoTimer = window.setTimeout($.proxy($this.expand, $this), $this.options.duration);
+		});
+		this.el.on("mouseleave", function () {
+			window.clearTimeout($this.enlargoTimer);
 		});
 		this.fullContainer.on("mouseleave", $.proxy(this.contract, this));
 	};
 
 	Enlargo.prototype.expand = function () {
+		var m = this.options.margin,
+			left,
+			top,
+			width,
+			height;
+
+		this.offset = this.el.offset();
+		this.fullContainer.css({
+			left: this.offset.left,
+			top: this.offset.top
+		});
+
+		left = this.offset.left - (this.fullWidth - this.width) / 2;
+		if (left + this.fullWidth > $(window).width() - m) {
+			left -= (left + this.fullWidth) - ($(window).width() - m);
+		}
+		top = this.offset.top - (this.fullHeight - this.height) / 2;
+
 		this.fullContainer.show().animate({
-			height: this.height * 3,
-			width: this.width * 3
+			height: this.fullHeight || this.height * 3,
+			left: left > m ? left : m,
+			top: top > m ? top : m,
+			width: this.fullWidth || this.width * 3
 		}, this.options.duration);
 	};
 
 	Enlargo.prototype.contract = function () {
 		this.fullContainer.animate({
 			height: this.height,
+			left: this.offset.left,
+			top: this.offset.top,
 			width: this.width
 		}, this.options.duration, function () {
 			$(this).hide();
